faf2d23166a9a549e8454c618b79ef56eff83ea5
adit <aditjain1980@gmail.com>
2020-12-21 12:51:36 +0530

    fixed blog articles

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ $(".tablinks").click(function(){
     $('#about-me ul').hide();
     $('#about-me div').hide();
     $('#'+ $(this).data("link")).show();
-    $('.tablinks').css({"border-bottom":"none","font-weight":"normal"});
-    $(this).css({"border-bottom":"1px solid black","font-weight":"bolder"});
+    $('.tablinks').css({"border":"none","font-weight":"normal"});
+    $(this).css({"border":"2px solid black","border-radius":"2px","font-weight":"bolder"});
 })
 $('#projects-button').click();
 particlesJS.load('background', 'data/particles.json', function() {
