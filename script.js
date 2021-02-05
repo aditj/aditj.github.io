5e77d4fce734e7b373528e80d28f17bf40873b53
adit <aditjain1980@gmail.com>
2021-02-05 19:02:09 +0530

    changed website

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ $(".tablinks").click(function(){
     $('#about-me ul').hide();
     $('#about-me div').hide();
     $('#'+ $(this).data("link")).show();
-    $('.tablinks').css({"border":"none","font-weight":"normal"});
-    $(this).css({"border":"2px solid black","border-radius":"2px","font-weight":"bolder"});
+    $('.tablinks').removeClass('selected-tab');
+    $(this).toggleClass("selected-tab");//.css({"border":"2px solid black","border-radius":"2px","font-weight":"bolder"});
 })
-$('#projects-button').click();
+$('.tablinks').first().click()
 particlesJS.load('background', 'data/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
